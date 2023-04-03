f70b4996d8fa327a5ce33c655b1c2d116c945cd8
izzmejeziiii <jeziannetobio970@gmail.com>
2023-04-03 10:19:34 +0800

    description

---
+++ chartConfig.js
@@ -0,0 +1,15 @@
+export const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July' ,'August', 'September', 'October', 'November', 'December'],
+    datasets: [
+      {
+        label: 'Montly Sales',
+        backgroundColor: '#0036fa',
+        data: [40, 39, 10, 40, 39, 80, 40, 80, 23, 100, 64, 20]
+      }
+    ]
+  }
+  
+  export const options = {
+    responsive: true,
+    maintainAspectRatio: false
+  }
---
+++ layout/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <div class="container mx-auto p-4">
+        <slot />
+    </div>
+  </div>
+  <AppHeader />
+  <AppSidebar />
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
